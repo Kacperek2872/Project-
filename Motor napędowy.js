eec930830ceccafd9e49ffd1010e4189e4e64b26
Kacperek2872 <lisowsky07@gmail.com>
2022-08-03 16:26:03 +0200

    lemee

---
+++ Motor napędowy.js
@@ -0,0 +1,19 @@
+const btn = document.querySelector("#Clickerdemo");
+const paragraf= document.querySelector("#demo");
+let show= true
+const paragraf2= document.querySelector("#demov2");
+btn.addEventListener ("click",function(){
+    // if(show==true){
+    //     show=false
+    // }
+    // else{
+    //     show=true
+    // } 
+    show = !show;
+    // paragraf.setAttribute('style','display:'+ show ? 'block' : 'none')
+    paragraf.style.display = show ? 'block' : 'none';
+    paragraf2.style.display = !show ? 'block' : 'none';
+    
+});
+paragraf2.style.display = 'none';
+//Wrzucić wszystko w funkcję i powielić o argument 
